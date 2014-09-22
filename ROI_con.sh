5eb4a93935b88c5596d50a854fdb3fd2875cccc8
kai.hwang@gmail.com <kaihwang@nx1.priv>
2014-09-22 09:37:19 -0700

    write out adj matrices.

--- ROI_con.sh
+++ ROI_con.sh
@@ -2,21 +2,23 @@
 
 #script to calculate ROIxROI connectivity matrices. Using AFNI
 
+# tha patients
 for s in 128 162 163 168; do
 	cd /home/despo/kaihwang/Rest/Lesion/${s}/Rest
 	
 	#create SNR mask.
-	rm ${s}_tsnr_mean.nii.gz
-	3dMean -prefix ${s}_tsnr_mean.nii.gz ${s}-preproc-run1.tsnr+tlrc ${s}-preproc-run2.tsnr+tlrc
-	rm ${s}_tsnr_mask.nii.gz
-	3dcalc -a ${s}_tsnr_mean.nii.gz -expr 'step(a-15)' -prefix ${s}_tsnr_mask.nii.gz 
+	#rm ${s}_tsnr_mean.nii.gz
+	#3dMean -prefix ${s}_tsnr_mean.nii.gz ${s}-preproc-run1.tsnr+tlrc ${s}-preproc-run2.tsnr+tlrc
+	#rm ${s}_tsnr_mask.nii.gz
+	#3dcalc -a ${s}_tsnr_mean.nii.gz -expr 'step(a-15)' -prefix ${s}_tsnr_mask.nii.gz 
 	
 	# connectivity
 	rm *corrmat*
 	
-	3dNetCorr -prefix ${s}_Full_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/Lesion/bb264_s.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
-	3dNetCorr -prefix ${s}_Right_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/Lesion/bb264_s_right.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
-	3dNetCorr -prefix ${s}_Left_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/Lesion/bb264_s_left.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	3dNetCorr -prefix ${s}_Full_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_TSNR_masked.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	3dNetCorr -prefix ${s}_Right_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_right_TSNR_masked.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	3dNetCorr -prefix ${s}_Left_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_left_TSNR_masked.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	
 	#ln -s /home/despo/kaihwang/Subjects/128/SUMA/128_MNI_final.nii.gz 128_MNI_final.nii.gz
 	#@ROI_Corr_Mat -ts ${s}-rest-preproc-cen.nii.gz -roi /home/despo/kaihwang/Rest/Lesion/bb264_s.nii.gz -mat FULL -prefix ${s}_Full_corrmat -verb
 	
@@ -25,8 +27,43 @@ for s in 128 162 163 168; do
 	#@ROI_Corr_Mat -ts ${s}-rest-preproc-cen.nii.gz -roi /home/despo/kaihwang/Rest/Lesion/bb264_s_left.nii.gz -mat FULL -prefix ${s}_left_corrmat -verb
 
 	
-	tail -n 128 ${s}_Left_corrmat_000.netcc > /home/despo/kaihwang/Rest/Lesion/${s}_left_corrmat
-	tail -n 143 ${s}_Right_corrmat_000.netcc > /home/despo/kaihwang/Rest/Lesion/${s}_right_corrmat
-	tail -n 254 ${s}_Full_corrmat_000.netcc > /home/despo/kaihwang/Rest/Lesion/${s}_full_corrmat
+	tail -n 126 ${s}_Left_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/t${s}_left_corrmat
+	tail -n 141 ${s}_Right_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/t${s}_right_corrmat
+	tail -n 250 ${s}_Full_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/t${s}_full_corrmat
+
+done
+
+
+# BG patients
+for s in b116 b117 b120 b121 b122 b138 b143 b153; do
+	cd /home/despo/kaihwang/Rest/BG/${s}/Rest
+	
+	# connectivity
+	rm *corrmat*
+	
+	3dNetCorr -prefix ${s}_Full_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_TSNR_masked.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	3dNetCorr -prefix ${s}_Right_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_right_TSNR_masked.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	3dNetCorr -prefix ${s}_Left_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_left_TSNR_masked.nii.gz -fish_z -mask ${s}_tsnr_mask.nii.gz
+	
+	tail -n 126 ${s}_Left_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/${s}_left_corrmat
+	tail -n 141 ${s}_Right_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/${s}_right_corrmat
+	tail -n 250 ${s}_Full_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/${s}_full_corrmat
+
+done
+
+# Control
+for s in 114 116 117 118 119 201 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220; do
+	cd /home/despo/kaihwang/Rest/Control/${s}/Rest
+	
+	# connectivity
+	rm *corrmat*
+	
+	3dNetCorr -prefix ${s}_Full_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_TSNR_masked.nii.gz -fish_z 
+	3dNetCorr -prefix ${s}_Right_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_right_TSNR_masked.nii.gz -fish_z
+	3dNetCorr -prefix ${s}_Left_corrmat -inset ${s}-rest-preproc-cen.nii.gz -in_rois /home/despo/kaihwang/Rest/ROIs/bb264_s_left_TSNR_masked.nii.gz -fish_z
+	
+	tail -n 126 ${s}_Left_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/c${s}_left_corrmat
+	tail -n 141 ${s}_Right_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/c${s}_right_corrmat
+	tail -n 250 ${s}_Full_corrmat_000.netcc > /home/despo/kaihwang/bin/Thalamo/Data/c${s}_full_corrmat
 
 done
--- cal_tsnr.sh
+++ cal_tsnr.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-for s in 128 162 163 168; do
-	cd /home/despo/kaihwang/Rest/Lesion/${s}/Rest
+# tha patients
+#for s in 128 162 163 168; do
+	#cd /home/despo/kaihwang/Rest/Lesion/${s}/Rest
+	#rm ${s}_tsnr_mean.nii.gz
+	#3dMean -prefix ${s}_tsnr_mean.nii.gz ${s}-preproc-run1.tsnr+tlrc ${s}-preproc-run2.tsnr+tlrc
+	#rm ${s}_tsnr_mask.nii.gz
+	#3dcalc -a ${s}_tsnr_mean.nii.gz -expr 'step(a-10)' -prefix ${s}_tsnr_mask.nii.gz
+	
+	
+#done
+
+# BG patients
+for s in b116 b117 b120 b121 b122 b138 b143 b153; do
+	cd /home/despo/kaihwang/Rest/BG/${s}/Rest
 	rm ${s}_tsnr_mean.nii.gz
 	3dMean -prefix ${s}_tsnr_mean.nii.gz ${s}-preproc-run1.tsnr+tlrc ${s}-preproc-run2.tsnr+tlrc
 	rm ${s}_tsnr_mask.nii.gz
@@ -9,3 +21,40 @@ for s in 128 162 163 168; do
 	
 	
 done
+
+# control
+for s in 114 116 117 118 119 201 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220; do
+	cd /home/despo/kaihwang/Rest/Control/${s}/Rest
+	rm ${s}_tsnr_mean.nii.gz
+	3dMean -prefix ${s}_tsnr_mean.nii.gz ${s}-preproc-run1.tsnr+tlrc ${s}-preproc-run2.tsnr+tlrc ${s}-preproc-run3.tsnr+tlrc ${s}-preproc-run4.tsnr+tlrc ${s}-preproc-run5.tsnr+tlrc ${s}-preproc-run6.tsnr+tlrc
+	#rm ${s}_tsnr_mask.nii.gz
+	#3dcalc -a ${s}_tsnr_mean.nii.gz -expr 'step(a-10)' -prefix ${s}_tsnr_mask.nii.gz
+	
+	
+done
+
+cd /home/despo/kaihwang/Rest/Control/
+
+3dMean -prefix control_tsnr_mean.nii.gz /home/despo/kaihwang/Rest/Control/114/Rest/114_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/116/Rest/116_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/117/Rest/117_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/118/Rest/118_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/119/Rest/119_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/201/Rest/201_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/203/Rest/203_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/204/Rest/204_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/205/Rest/205_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/206/Rest/206_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/207/Rest/207_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/208/Rest/208_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/209/Rest/209_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/210/Rest/210_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/211/Rest/211_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/212/Rest/212_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/213/Rest/213_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/214/Rest/214_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/216/Rest/216_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/217/Rest/217_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/218/Rest/218_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/219/Rest/219_tsnr_mean.nii.gz \
+/home/despo/kaihwang/Rest/Control/220/Rest/220_tsnr_mean.nii.gz
