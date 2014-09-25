a8a08fcaa6f7e96f57ab17a48a20e3107cb9280e
kai.hwang@gmail.com <kaihwang@nx4.priv>
2014-09-25 15:26:13 -0700

    do efficiency.

---
+++ cal_efficiency.m
@@ -0,0 +1,93 @@
+% Script to load data.
+
+%% 
+
+
+
+%load data
+cd /home/despo/kaihwang/Rest
+%load Data_500.mat
+DATASET{1}='Data.mat';
+DATASET{2}='Data_500.mat';
+DATASET{3}='Data_100.mat';
+
+%threshold
+T = 0.05:0.005:0.15;
+
+
+for c = 1:3
+    load(DATASET{c});
+    
+    n=1;
+    for s = Th_Sub
+        i=1;
+        for th = T
+            M = squeeze(M_th(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_tha(c,n,i) = E;
+            
+            M = squeeze(M_th_R(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_tha_R(c,n,i) = E;
+            
+            M = squeeze(M_th_L(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_tha_L(c,n,i) = E;
+
+            i=i+1;
+        end
+        n=n+1;
+    end
+    
+    n=1;
+    for s = BG_Sub
+        i=1;
+        for th = T
+            M = squeeze(M_BG(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_BG(c,n,i) = E;
+            
+            M = squeeze(M_BG_R(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_BG_R(c,n,i) = E;
+            
+            M = squeeze(M_BG_L(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_BG_L(c,n,i) = E;
+
+            i=i+1;
+        end
+        n=n+1;
+    end
+    
+    n=1;
+    for s = C_Sub
+        i=1;
+        for th = T
+            
+            M = squeeze(M_c(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_Control(c,n,i) = E;
+            
+            M = squeeze(M_c_R(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_Control_R(c,n,i) = E;
+            
+            M = squeeze(M_c_L(:,:,n));
+            [E] = efficiency_bin(weight_conversion(threshold_proportional(M,th),'binarize'));
+            Efficiency_Control_L(c,n,i) = E;
+
+            i=i+1;
+        end
+        n=n+1;
+    end
+    
+end
+
+clear c n i th s E M
+save efficiency.mat
+
+
+
+
+
--- make_plots.m
+++ make_plots.m
@@ -1,3 +1,4 @@
+%% plot modularity
 close all
 figure
 H1=shadedErrorBar(T,squeeze(Modularity_Control(2,:,:)),{@mean, @std},{'-','LineWidth',6,'Color',rgb('grey')},1);
@@ -53,3 +54,61 @@ ylabel('Diff in Modularity','FontSize',24)
 title('Hemispheric Diff','FontSize',24)
 set(gcf, 'Color', 'white');
 export_fig Modularity_hemDiff.png
+
+%% plot efficiency
+
+close all
+figure
+H1=shadedErrorBar(T,squeeze(Efficiency_Control(2,:,:)),{@mean, @std},{'-','LineWidth',6,'Color',rgb('grey')},1);
+hold on;
+H2=plot(T,squeeze(Efficiency_tha(2,1,:)),'linewidth',3,'color','b');
+H3=plot(T,squeeze(Efficiency_tha(2,2,:)),'linewidth',3,'color','r');
+H4=plot(T,squeeze(Efficiency_tha(2,3,:)),'linewidth',3,'color','g');
+H5=plot(T,squeeze(Efficiency_tha(2,4,:)),'linewidth',3,'color','y');
+xlim([0.05 0.16])
+ylim([0.25 0.65])
+hl=legend([H1.mainLine,H2, H3, H4, H5],'Controls ','Patient 128','Patient 162', 'Patient 163', 'Patient 168','Location','Southeast' );
+set(gca,'FontSize',16,'box','off','XGrid','off','YGrid','off','linewidth',10)
+set(hl,'FontSize', 12, 'Box','off');
+xlabel('Cost','FontSize',24)
+ylabel('Efficiency','FontSize',24)
+title('Whole Brain Efficiency','FontSize',24)
+set(gcf, 'Color', 'white');
+export_fig Efficiency.png; 
+
+figure
+H1=shadedErrorBar(T,squeeze(Efficiency_Control_L(2,:,:)),{@mean, @std},{'-','LineWidth',6,'Color',rgb('grey')},1);
+hold on;
+H2=shadedErrorBar(T,squeeze(Efficiency_Control_R(2,:,:)),{@mean, @std},{'--','LineWidth',6,'Color',rgb('grey')},1);
+H3=plot(T,squeeze(Efficiency_tha_L(2,1,:)),'linewidth',3,'color','b');
+H4=plot(T,squeeze(Efficiency_tha_R(2,2,:)),'linewidth',3,'color','r');
+H5=plot(T,squeeze(Efficiency_tha_L(2,3,:)),'linewidth',3,'color','g');
+H6=plot(T,squeeze(Efficiency_tha_R(2,4,:)),'linewidth',3,'color','y');
+xlim([0.05 0.16])
+ylim([0.25 0.65])
+hl=legend([H1.mainLine,H2.mainLine,H3, H4, H5, H6],'Controls Left','Controls Right','Patient 128','Patient 162', 'Patient 163', 'Patient 168','Location','best' );
+set(gca,'FontSize',16,'box','off','XGrid','off','YGrid','off','linewidth',10)
+set(hl,'FontSize', 12, 'Box','off');
+xlabel('Cost','FontSize',24)
+ylabel('Efficiency','FontSize',24)
+title('Lesioned Hemisphere Efficiency','FontSize',24)
+set(gcf, 'Color', 'white');
+export_fig Efficiency_lesioned_hemisphere.png
+
+figure
+H1=shadedErrorBar(T,squeeze(Efficiency_Control_L(2,:,:)-Efficiency_Control_R(2,:,:)),{@mean, @std},{'-','LineWidth',6,'Color',rgb('grey')},1);
+hold on;
+H3=plot(T,squeeze(Efficiency_tha_L(2,1,:)-Efficiency_tha_R(2,1,:)),'linewidth',3,'color','b');
+H4=plot(T,squeeze(Efficiency_tha_R(2,2,:)-Efficiency_tha_L(2,2,:)),'linewidth',3,'color','r');
+H5=plot(T,squeeze(Efficiency_tha_L(2,3,:)-Efficiency_tha_R(2,3,:)),'linewidth',3,'color','g');
+H6=plot(T,squeeze(Efficiency_tha_R(2,4,:)-Efficiency_tha_L(2,4,:)),'linewidth',3,'color','y');
+xlim([0.05 0.16])
+%ylim([0.25 0.65])
+hl=legend([H1.mainLine,H3, H4, H5, H6],'Controls L-R','Patient 128 Lesioned - Intact','Patient 162 Lesioned - Intact', 'Patient 163 Lesioned - Intact', 'Patient 168 Lesioned - Intact','Location','best' );
+set(gca,'FontSize',16,'box','off','XGrid','off','YGrid','off','linewidth',10)
+set(hl,'FontSize', 12, 'Box','off');
+xlabel('Cost','FontSize',24)
+ylabel('Diff in Efficiency','FontSize',24)
+title('Hemispheric Diff','FontSize',24)
+set(gcf, 'Color', 'white');
+export_fig Efficiency_hemDiff.png
