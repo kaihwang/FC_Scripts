bcfb74a5537b1db0b350ce74a139677c0bc79c17
kai.hwang@gmail.com <kaihwang@nx1.priv>
2014-09-30 22:22:50 -0700

    submit matlab jobs.

--- tmp/cc_162.sh
+++
@@ -1,8 +0,0 @@
-cd /home/despo/kaihwang/Rest/Lesion/162/Rest
-
-3dTcat -prefix 162-rest-preproc-cen.nii.gz -rlt \
-	reg_run1/162-preproc-run1-censored+tlrc \
-	reg_run2/162-preproc-run2-censored+tlrc
-
-mv reg*/*tsnr* .
-rm -rf reg_run*
