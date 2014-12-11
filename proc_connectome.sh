8c23a42225517a79a63b689e1790b3e258a95278
kai.hwang@gmail.com <kaihwang@nx1.priv>
2014-12-11 10:15:19 -0800

    changes to process full data from connectome-in-a-box

--- proc_connectome.sh
+++ proc_connectome.sh
@@ -4,63 +4,56 @@
 # here we will take the minimally preprocessed connectome data, and run it through our typical AFNI pipeline.
 
 
-WD='/home/despo/kaihwang/Rest/Connectome'
+WD='/home/despoB/connectome-thalamus'
+RAW='/home/despoB/connectome-raw'
 
 for s in 100307; do
-
-	for run in rfMRI_REST1_LR  rfMRI_REST1_RL  ; do #rfMRI_REST2_LR  rfMRI_REST2_RL will be used for validation
 	
-		#rm -rf ${WD}/${s}/MNINonLinear/Results/${run}/Preproc/
-		cd ${WD}/${s}/MNINonLinear/Results/${run}/
-				
-		rm ${WD}/${s}/MNINonLinear/Results/${run}/${run}_input*
-		rm __*
-		#rm ${WD}/${s}/MNINonLinear/Results/${run}/anat*
-		rm ${WD}/${s}/MNINonLinear/Results/${run}/aseg*
+	mkdir ${WD}/connectome/${s}/
+	mkdir ${WD}/connectome/${s}/MNINonLinear/
+	cd {WD}/connectome/${s}/MNINonLinear/
+	
+	#copy structural files and segmentations
+	3dcopy ${RAW}/${s}/MNINonLinear/aparc+aseg.nii.gz {WD}/connectome/${s}/MNINonLinear/aseg
+	3drefit -view tlrc {WD}/connectome/${s}/MNINonLinear/aseg+orig
+	
+	# bunc of T1s no idea what is what at this point...
+	ln -s ${RAW}/${s}/MNINonLinear/T1w_restore.nii.gz {WD}/connectome/${s}/MNINonLinear/T1w_restore.nii.gz
+	ln -s ${RAW}/${s}/MNINonLinear/T1w.nii.gz {WD}/connectome/${s}/MNINonLinear/T1w.nii.gz
+	ln -s ${RAW}/${s}/MNINonLinear/T1w_restore.2.nii.gz {WD}/connectome/${s}/MNINonLinear/T1w_restore.2.nii.gz
+	ln -s ${RAW}/${s}/MNINonLinear/T1w_restore_brain.nii.gz {WD}/connectome/${s}/MNINonLinear/T1w_restore_brain.nii.gz
+	
+	for run in rfMRI_REST1_LR  rfMRI_REST1_RL rfMRI_REST2_LR  rfMRI_REST2_RL  ; do #rfMRI_REST2_LR  rfMRI_REST2_RL will be used for validation
 		
-		3dcopy ${WD}/${s}/MNINonLinear/Results/${run}/${run}.nii.gz ${WD}/${s}/MNINonLinear/Results/${run}/${run}_input
-		#3dcopy /usr/local/fsl-5.0.6/data/standard/MNI152lin_T1_1mm.nii.gz ${WD}/${s}/MNINonLinear/Results/${run}/anat
-		#3drefit -view orig ${WD}/${s}/MNINonLinear/Results/${run}/anat+tlrc
-		3dcopy ${WD}/${s}/MNINonLinear/aparc+aseg.nii.gz ${WD}/${s}/MNINonLinear/Results/${run}/aseg
-		3drefit -view tlrc ${WD}/${s}/MNINonLinear/Results/${run}/aseg+orig
-		cp ${WD}/${s}/MNINonLinear/Results/${run}/Movement_Regressors_dt.txt ${WD}/${s}/MNINonLinear/Results/${run}/mopar.1D
+		#create symbolic link of functional data and regressors for motion data
+		ln -s ${RAW}/${s}/MNINonLinear/Results/${run}/${run}.nii.gz {WD}/connectome/${s}/MNINonLinear/${run}.nii.gz
+		cp ${WD}/${s}/MNINonLinear/Results/${run}/Movement_Regressors_dt.txt ${WD}/connectome/${s}/MNINonLinear/${run}_mopar.1D
+		
+		#remove previous version
+		#rm ${WD}/${s}/MNINonLinear/Results/${run}/${run}_input*
+		#rm __*
+		#rm ${WD}/${s}/MNINonLinear/Results/${run}/anat*
+		#rm ${WD}/${s}/MNINonLinear/Results/${run}/aseg*
+		
+		3dcopy {WD}/connectome/${s}/MNINonLinear/${run}.nii.gz ${WD}/connectome/${s}/MNINonLinear/${run}_input
 		
 		@ANATICOR \
-		-ts ${run}_input+tlrc \
+		-ts ${WD}/connectome/${s}/MNINonLinear/${run}_input \
 		-polort 3 \
-		-aseg aseg+tlrc \
-		-motion mopar.1D \
-		-prefix ${run}_reg \
+		-aseg {WD}/connectome/${s}/MNINonLinear/aseg+tlrc \
+		-motion ${WD}/connectome/${s}/MNINonLinear/${run}_mopar.1D \
+		-prefix  ${WD}/connectome/${s}/MNINonLinear/${run}_reg \
 		-view +tlrc
-		#-echo
 		
 		3dBandpass \
 		-nodetrend -automask \
 		-blur 5 -band 0.009 0.08 \
-		-prefix ${run}_reg_bp.nii.gz \
-		-input ${run}_reg+tlrc 
+		-prefix ${WD}/connectome/${s}/MNINonLinear/${run}_reg_bp.nii.gz \
+		-input ${WD}/connectome/${s}/MNINonLinear/${run}_reg+tlrc 
+				
+		rm ${WD}/connectome/${s}/MNINonLinear/${run}_reg+tlrc* 
+		rm ${WD}/connectome/${s}/MNINonLinear/${run}_input*
 		
-		#afni_restproc.py \
-		#-epi ${WD}/${s}/MNINonLinear/Results/${run}/${run}_input+tlrc \
-		#-anat ${WD}/${s}/MNINonLinear/Results/${run}/anat+tlrc \
-		#-prefix ${WD}/${s}/MNINonLinear/Results/${run}/${s}_${run}.nii.gz \
-		#-dest ${WD}/${s}/MNINonLinear/Results/${run}/Preproc/ \
-		#-align off \
-		#-aseg ${WD}/${s}/MNINonLinear/Results/${run}/aseg+tlrc \
-		#-regressor ${WD}/${s}/MNINonLinear/Results/${run}/Movement_Regressors.txt \
-		#-globalnorm \
-		#-smoothrad 5 \
-		#-despike off \
-		#-bandpass \
-		#-bpassregs \
-		#-script cmmd \
-		#-trcut 0 \
-		#-tsnr 
-		
-		rm ${WD}/${s}/MNINonLinear/Results/${run}/${run}_input*
-		#rm ${WD}/${s}/MNINonLinear/Results/${run}/${run}_reg+tlrc*
-		
-		#rm ${WD}/${s}/MNINonLinear/Results/${run}/anat*
 	done
 
 done
--- qsub_script.sh
+++ qsub_script.sh
@@ -6,8 +6,7 @@ mkdir tmp;
 
 #cd /home/despo/kaihwang/Rest/Connectome
 
-for Subject in $(ls /home/despo/kaihwang/Rest/Connectome/* -d | grep -Eo '[0-9]{1,6}'); do
+for Subject in $(ls /home/despoB/connectome-raw/* -d | grep -Eo '[0-9]{1,6}'); do
 	sed "s/s in 100307/s in ${Subject}/g" < proc_connectome.sh > tmp/q_proc_connectome_${Subject}.sh
 	qsub -V -M kaihwang -m e -l mem_free=10G -e ~/tmp -o ~/tmp tmp/q_proc_connectome_${Subject}.sh
-	wait 3.33m
 done
