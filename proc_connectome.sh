17a0b794ac365e463350bf4b3974400605734505
kaihwang <kai.hwang@gmail.com>
2014-12-22 15:34:27 -0800

    write intermediate output files to /tmp to speed to I/O.

--- proc_connectome.sh
+++ proc_connectome.sh
@@ -29,25 +29,29 @@ for s in 100307; do
 	ln -s ${RAW}/${s}/MNINonLinear/T1w_restore.2.nii.gz ${WD}/connectome/${s}/MNINonLinear/T1w_restore.2.nii.gz
 	ln -s ${RAW}/${s}/MNINonLinear/T1w_restore_brain.nii.gz ${WD}/connectome/${s}/MNINonLinear/T1w_restore_brain.nii.gz
 	
+	# create temp output folder
+	mkdir /tmp/${s}
+
 	for run in rfMRI_REST1_LR  rfMRI_REST1_RL rfMRI_REST2_LR  rfMRI_REST2_RL  ; do #rfMRI_REST2_LR  rfMRI_REST2_RL will be used for validation
 		
 		#create symbolic link of functional data and regressors for motion data
 		ln -s ${RAW}/${s}/MNINonLinear/Results/${run}/${run}.nii.gz ${WD}/connectome/${s}/MNINonLinear/${run}.nii.gz
 		cp ${RAW}/${s}/MNINonLinear/Results/${run}/Movement_Regressors_dt.txt ${WD}/connectome/${s}/MNINonLinear/${run}_mopar.1D
 		
-		#remove previous version
-		#rm ${WD}/${s}/MNINonLinear/Results/${run}/${run}_input*
-		#rm __*
-		#rm ${WD}/${s}/MNINonLinear/Results/${run}/anat*
-		#rm ${WD}/${s}/MNINonLinear/Results/${run}/aseg*
-		
-		3dcopy ${WD}/connectome/${s}/MNINonLinear/${run}.nii.gz ${WD}/connectome/${s}/MNINonLinear/${run}_input
-		
+		# Per Julie's suggestion. Do the calculations in /tmp
+		mkdir /tmp/${s}/${run}
+
+		cd /tmp/${s}/${run}
+
+		3dcopy ${WD}/connectome/${s}/MNINonLinear/${run}.nii.gz /tmp/${s}/${run}/${run}_input
+		cp ${WD}/connectome/${s}/MNINonLinear/aseg+tlrc* /tmp/${s}/${run}/
+		cp ${WD}/connectome/${s}/MNINonLinear/${run}_mopar.1D /tmp/${s}/${run}/
+
 		@ANATICOR \
-		-ts ${WD}/connectome/${s}/MNINonLinear/${run}_input+tlrc \
+		-ts /tmp/${s}/${run}/${run}_input+tlrc \
 		-polort 3 \
-		-aseg ${WD}/connectome/${s}/MNINonLinear/aseg+tlrc \
-		-motion ${WD}/connectome/${s}/MNINonLinear/${run}_mopar.1D \
+		-aseg /tmp/${s}/${run}/aseg+tlrc \
+		-motion /tmp/${s}/${run}/${run}_mopar.1D \
 		-prefix ${run}_reg \
 		-view +tlrc
 		
@@ -55,11 +59,13 @@ for s in 100307; do
 		-nodetrend -automask \
 		-blur 5 -band 0.009 0.08 \
 		-prefix ${WD}/connectome/${s}/MNINonLinear/${run}_reg_bp.nii.gz \
-		-input ${WD}/connectome/${s}/MNINonLinear/${run}_reg+tlrc 
+		-input $/tmp/${s}/${run}/${run}_reg+tlrc 
 				
-		rm ${WD}/connectome/${s}/MNINonLinear/${run}_reg+tlrc* 
-		rm ${WD}/connectome/${s}/MNINonLinear/${run}_input*
-		
-	done
+		#rm ${WD}/connectome/${s}/MNINonLinear/${run}_reg+tlrc* 
+		#rm ${WD}/connectome/${s}/MNINonLinear/${run}_input*
+		rm -rf /tmp/${s}/${run}
 
+
+	done
+	rm -rf /tmp/${s}/
 done
