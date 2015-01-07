3058e6beb18af9ddc46413ff804fe0788f46ddd7
kaihwang <kai.hwang@gmail.com>
2015-01-07 12:39:00 -0800

    fix filename typo.

--- proc_connectome_with_afni_proc.sh
+++ proc_connectome_with_afni_proc.sh
@@ -7,11 +7,10 @@ WD='/home/despoB/connectome-thalamus'
 # 105216  113215  120515  128127  133928  140824  148032  153429  159340  164939  172332  179346  188347  195849 100408  106016  113619  121315  128329  134324  140925  148335  153833  159441  165032  172534  179548  189349  196144
 for s in 100307 ; do
 
-	cd $WD/connectome/${s}/MNINonLinear/
-	
-
 	for run in rfMRI_REST1_LR  rfMRI_REST1_RL rfMRI_REST2_LR  rfMRI_REST2_RL  ; do
 		
+		cd $WD/connectome/${s}/MNINonLinear/
+
 		mkdir /tmp/KH_${s}_${run}
 
 		3dcopy T1w_restore_brain.nii.gz anat
@@ -19,7 +18,7 @@ for s in 100307 ; do
 		
 		cp ${run}_mopar.1D /tmp/KH_${s}_${run}
 		cp aseg+tlrc* /tmp/KH_${s}_${run}
-		
+
 		3dcopy ${run}.nii.gz ${run}_input
 		mv ${run}_input* /tmp/KH_${s}_${run}
 		
@@ -41,6 +40,8 @@ for s in 100307 ; do
 		-bpassregs \
 		-script proc_test \
 		-prefix ${run}_gwreg_bp
+
+		3dAFNItoNIFTI -prefix $WD/connectome/${s}/MNINonLinear/${run}_gwreg_bp.nii.gz ${run}_gwreg_bp.cleanEPI+tlrc
 
 	done
 
