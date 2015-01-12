2c16319c50f4f448bbf15feac7b3f1e84bc06b0e
kaihwang <kai.hwang@gmail.com>
2015-01-12 09:49:31 -0800

    change memory req for SGE jobs.

--- qsub_rest.sh
+++ qsub_rest.sh
@@ -7,7 +7,7 @@
 for Subject in 128 162 163 168 176; do
 
 		sed "s/s in 128/s in ${Subject}/g" < proc_th_HCPpreproc_reg_bp.sh> tmp/q_proc_th_HCPpreproc_reg_bp_${Subject}.sh
-		qsub -V -M kaihwang -m e -l mem_free=8G -e ~/tmp -o ~/tmp tmp/q_proc_th_HCPpreproc_reg_bp_${Subject}.sh
+		qsub -V -M kaihwang -m e -l mem_free=5G -e ~/tmp -o ~/tmp tmp/q_proc_th_HCPpreproc_reg_bp_${Subject}.sh
 
 
 done
@@ -15,7 +15,7 @@ done
 for Subject in b116 b117 b120 b121 b122 b138 b143 b144 b153; do
 
 		sed "s/s in 128/s in ${Subject}/g" < proc_bg_HCPpreproc_reg_bp.sh> tmp/q_proc_bg_HCPpreproc_reg_bp_${Subject}.sh
-		qsub -V -M kaihwang -m e -l mem_free=8G -e ~/tmp -o ~/tmp tmp/q_proc_bg_HCPpreproc_reg_bp_${Subject}.sh
+		qsub -V -M kaihwang -m e -l mem_free=5G -e ~/tmp -o ~/tmp tmp/q_proc_bg_HCPpreproc_reg_bp_${Subject}.sh
 
 
 done
@@ -23,7 +23,7 @@ done
 for Subject in 114 117 119 203 205 207 209 211 213 215 217 219 116 118 201 204 206 208 210 212 214 216 218 220; do
 
 		sed "s/s in 128/s in ${Subject}/g" < proc_c_HCPpreproc_reg_bp.sh> tmp/q_proc_c_HCPpreproc_reg_bp_${Subject}.sh
-		qsub -V -M kaihwang -m e -l mem_free=8G -e ~/tmp -o ~/tmp tmp/q_proc_c_HCPpreproc_reg_bp_${Subject}.sh
+		qsub -V -M kaihwang -m e -l mem_free=5G -e ~/tmp -o ~/tmp tmp/q_proc_c_HCPpreproc_reg_bp_${Subject}.sh
 
 
 done
