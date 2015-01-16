dfd39089c0312588c702e61a1153ed1cb77212d1
Kai Hwang <kaihwang@Kais-MacBook-Pro.local>
2015-01-16 11:43:22 -0800

    add conditional if to prevent rerun FS.

--- qsub_script.sh
+++ qsub_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir tmp;
-
+export SUBJECTS_DIR="/home/despoB/kaihwang/Subjects"
 WD='/home/despoB/kaihwang/Rest/Older_Controls'
 SCRIPT='/home/despoB/kaihwang/bin/Thalamo'
 
@@ -11,9 +11,9 @@ cd ${WD}
 for Subject in $(ls -d 1*); do
 #for Subject in 268850; do	
 	
-	#if [ ! -d "${WD}/connectome/${Subject}/" ]; then
+	if [ ! -d "${SUBJECTS_DIR}/${Subject}/" ]; then
 		sed "s/s in 1103/s in ${Subject}/g" < ${SCRIPT}/q_ana_preproc.sh > ${SCRIPT}/tmp/anatomical_${Subject}.sh
 		qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp ${SCRIPT}/tmp/anatomical_${Subject}.sh  #-l mem_free=5G
-	#fi
+	fi
 
 done
