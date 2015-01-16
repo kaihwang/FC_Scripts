d9953460ff2295d3e65d9e0e38791bf2e9f0938b
Kai Hwang <kaihwang@Kais-MacBook-Pro.local>
2015-01-16 10:18:44 -0800

    run anatomical pipelines on older subjects.

--- qsub_script.sh
+++ qsub_script.sh
@@ -2,16 +2,17 @@
 
 mkdir tmp;
 
-WD='/home/despoB/connectome-thalamus'
+WD='/home/despoB/kaihwang/Rest/Older_Controls'
 
 #cd /home/despo/kaihwang/Rest/Connectome
+cd ${WD}
 
-for Subject in $(cat /home/despoB/kaihwang/Rest/connectome/list_of_complete_subjects); do
+for Subject in $(ls -d 1*); do
 #for Subject in 268850; do	
 	
 	#if [ ! -d "${WD}/connectome/${Subject}/" ]; then
-		sed "s/s in 100307/s in ${Subject}/g" < do_333ROI_con_graph_connectome.sh > tmp/do_333ROI_con_graph_${Subject}.sh
-		qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/do_333ROI_con_graph_${Subject}.sh  #-l mem_free=5G
+		sed "s/ss in 1103/s in ${Subject}/g" < q_ana_preproc.sh > tmp/anatomical_${Subject}.sh
+		qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/anatomical_${Subject}.sh  #-l mem_free=5G
 	#fi
 
 done
