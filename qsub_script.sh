93c0d2e1f4c411de228c3cf7e9b9d893f1b2b4fd
Kai Hwang <kaihwang@Kais-MacBook-Pro.local>
2015-01-16 10:39:17 -0800

    script dir.

--- qsub_script.sh
+++ qsub_script.sh
@@ -3,6 +3,7 @@
 mkdir tmp;
 
 WD='/home/despoB/kaihwang/Rest/Older_Controls'
+SCRIPT='/home/despoB/kaihwang/bin/Thalamo'
 
 #cd /home/despo/kaihwang/Rest/Connectome
 cd ${WD}
@@ -11,8 +12,8 @@ for Subject in $(ls -d 1*); do
 #for Subject in 268850; do	
 	
 	#if [ ! -d "${WD}/connectome/${Subject}/" ]; then
-		sed "s/ss in 1103/s in ${Subject}/g" < q_ana_preproc.sh > tmp/anatomical_${Subject}.sh
-		qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/anatomical_${Subject}.sh  #-l mem_free=5G
+		sed "s/ss in 1103/s in ${Subject}/g" < ${SCRIPT}/q_ana_preproc.sh > ${SCRIPT}/tmp/anatomical_${Subject}.sh
+		qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp ${SCRIPT}/tmp/anatomical_${Subject}.sh  #-l mem_free=5G
 	#fi
 
 done
