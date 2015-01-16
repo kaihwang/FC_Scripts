a0cd02fcda3c02da746e259334c041e70baeec42
Kai Hwang <kaihwang@Kais-MacBook-Pro.local>
2015-01-16 11:13:06 -0800

    remove additional "s"

--- qsub_script.sh
+++ qsub_script.sh
@@ -12,7 +12,7 @@ for Subject in $(ls -d 1*); do
 #for Subject in 268850; do	
 	
 	#if [ ! -d "${WD}/connectome/${Subject}/" ]; then
-		sed "s/ss in 1103/s in ${Subject}/g" < ${SCRIPT}/q_ana_preproc.sh > ${SCRIPT}/tmp/anatomical_${Subject}.sh
+		sed "s/s in 1103/s in ${Subject}/g" < ${SCRIPT}/q_ana_preproc.sh > ${SCRIPT}/tmp/anatomical_${Subject}.sh
 		qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp ${SCRIPT}/tmp/anatomical_${Subject}.sh  #-l mem_free=5G
 	#fi
 
