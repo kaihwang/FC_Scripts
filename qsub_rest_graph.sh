4c3d6accfe67c297f3f499c3b4af5c11e75fc45e
kai.hwang@gmail.com <kaihwang@nx1.priv>
2015-01-17 20:42:33 -0800

    ..

--- qsub_rest_graph.sh
+++ qsub_rest_graph.sh
@@ -14,17 +14,19 @@
 
 for Subject in 114 117 119 203 205 207 209 211 213 215 217 219 116 118 201 204 206 208 210 212 214 216 218 220; do
 
+	if [ ! -a "~/Rest/Graph/g_${Subject}.mat" ]; then
 	sed "s/s in 128/s in ${Subject}/g" < do_333ROI_con_graph_control.sh > tmp/do_333ROI_con_graph_control_${Subject}.sh
 	qsub -V -M kaihwang -l mem_free=14G -m e -e ~/tmp -o ~/tmp tmp/do_333ROI_con_graph_control_${Subject}.sh
-
+	fi
 
 done
 
 
-#for Subject in $(cat /home/despoB/kaihwang/Rest/connectome/list_of_complete_subjects); do
+for Subject in $(cat /home/despoB/kaihwang/Rest/connectome/list_of_complete_subjects); do
 
-#	sed "s/s in 100307/s in ${Subject}/g" < do_333ROI_con_graph_connectome.sh > tmp/do_333ROI_con_graph_connectome_${Subject}.sh
-#	qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/do_333ROI_con_graph_connectome_${Subject}.sh
+	if [ ! -a "~/Rest/Graph/g_${Subject}.mat" ]; then
+	sed "s/s in 100307/s in ${Subject}/g" < do_333ROI_con_graph_connectome.sh > tmp/do_333ROI_con_graph_connectome_${Subject}.sh
+	qsub -V -M kaihwang -l mem_free=13G -m e -e ~/tmp -o ~/tmp tmp/do_333ROI_con_graph_connectome_${Subject}.sh
+	fi
 
-
-#done
+done
