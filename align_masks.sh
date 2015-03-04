1fd0e11e5cd8c770781f178db186cb0ff55789b5
Kai Hwang <kai.hwang@gmail.com>
2015-03-04 12:55:55 -0800

    use fslorient.

--- align_masks.sh
+++ align_masks.sh
@@ -10,10 +10,10 @@ for s in 128; do
 
 	cd ${WD}
 
-
-	3dresample -inset ${s}_native_lesion_mask.nii.gz \
-	-master /home/despoB/kaihwang/Subjects/${s}/SUMA/${s}_SurfVol_biascorr.nii.gz \
-	-prefix ${s}_native_lesion_mask_RPI.nii.gz
+	#3dresample -inset ${s}_native_lesion_mask.nii.gz \
+	#-master /home/despoB/kaihwang/Subjects/${s}/SUMA/${s}_SurfVol_biascorr.nii.gz \
+	#-prefix ${s}_native_lesion_mask_RPI.nii.gz
+	fslreorient2std ${s}_native_lesion_mask.nii.gz ${s}_native_lesion_mask.nii.gz
 
 	applywarp --interp=nn --in=${s}_native_lesion_mask_RPI.nii.gz \
 	--ref=/home/despoB/kaihwang/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c_2mm \
