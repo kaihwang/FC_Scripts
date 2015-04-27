cf6be9762489c5a8da40eadb28fe57a58f4f366e
kaihwang <kai.hwang@gmail.com>
2015-04-27 14:37:12 -0700

    do striatal mapping with MGH data. :

--- do_striatalcortical_graph_MGH.sh
+++ do_striatalcortical_graph_MGH.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# script to create adj matrices using WashU's 333 ROI partition. Then submit to graph analysis. MGH Subs
+# script to create adj matrices using WashU's 333 ROI partition. Then submit to graph analysis. Mgh Subs
 
 
 WD='/home/despoB/connectome-thalamus/MGH/'
@@ -19,13 +19,13 @@ for s in Sub0001_Ses1; do
 
 	# run NetCorr
 	cd /tmp/KH_${s}
-	3dNetCorr -prefix ${s}_thalamocortical_corrmat -inset input.nii.gz -in_rois /home/despoB/kaihwang/Rest/ROIs/WashU297_plus_thalamus_25prob_voxels_ROIset_RPI.nii.gz
+	3dNetCorr -prefix ${s}_striatalcortical_corrmat -inset input.nii.gz -in_rois /home/despoB/kaihwang/Rest/ROIs/WashU297_plus_striatum_25prob_voxels_ROIset_RPI.nii.gz
 	#3dNetCorr -prefix ${s}_Right_corrmat -inset input.nii.gz -in_rois /home/despoB/kaihwang/Rest/ROIs/Craddock700_Cortical_R_2mm.nii.gz
 	#3dNetCorr -prefix ${s}_Left_corrmat -inset input.nii.gz -in_rois /home/despoB/kaihwang/Rest/ROIs/Craddock700_Cortical_L_2mm.nii.gz
 
 	# exract adj matrices
-	num=$(expr $(wc -l ${s}_thalamocortical_corrmat_000.netcc | awk '{print $1}') - 4)
-	tail -n $num ${s}_thalamocortical_corrmat_000.netcc > /home/despoB/kaihwang/Rest/AdjMatrices/t${s}_FIX_thalamocortical_corrmat
+	num=$(expr $(wc -l ${s}_striatalcortical_corrmat_000.netcc | awk '{print $1}') - 4)
+	tail -n $num ${s}_striatalcortical_corrmat_000.netcc > /home/despoB/kaihwang/Rest/AdjMatrices/t${s}_FIX_striatalcortical_corrmat
 
 	#num=$(expr $(wc -l ${s}_Right_corrmat_000.netcc | awk '{print $1}') - 4)
 	#tail -n $num ${s}_Right_corrmat_000.netcc > /home/despoB/kaihwang/Rest/AdjMatrices/t${s}_Right_Craddock700_corrmat
@@ -34,12 +34,12 @@ for s in Sub0001_Ses1; do
 	#tail -n $num ${s}_Left_corrmat_000.netcc > /home/despoB/kaihwang/Rest/AdjMatrices/t${s}_Left_Craddock700_corrmat
 		
 	#graph theory
-	echo "addpath(genpath('/home/despoB/kaihwang/bin/'));" >> thalamocorticalROIg${s}.m
-	echo "addpath(genpath('/home/despoB/kaihwang/matlab/'));" >> thalamocorticalROIg${s}.m
-	echo "[Adj, Graph] = cal_thalamo_cortical_graph('${s}');" >> thalamocorticalROIg${s}.m
-	echo "save /home/despoB/kaihwang/Rest/Thalamic_parcel/g_${s}.mat; exit;" >> thalamocorticalROIg${s}.m
+	echo "addpath(genpath('/home/despoB/kaihwang/bin/'));" >> striatalcorticalROIg${s}.m
+	echo "addpath(genpath('/home/despoB/kaihwang/matlab/'));" >> striatalcorticalROIg${s}.m
+	echo "[Adj, Graph] = cal_striatal_cortical_graph('${s}');" >> striatalcorticalROIg${s}.m
+	echo "save /home/despoB/kaihwang/Rest/Striatum_parcel/g_FIX_${s}.mat; exit;" >> striatalcorticalROIg${s}.m
 
-	matlab -nodisplay -nosplash < /tmp/KH_${s}/thalamocorticalROIg${s}.m
+	matlab -nodisplay -nosplash < /tmp/KH_${s}/striatalcorticalROIg${s}.m
 
 	cd ${WD}/${s}
 	rm -rf /tmp/KH_${s}/
--- qsub_thalamocortical.sh
+++ qsub_thalamocortical.sh
@@ -7,8 +7,8 @@ WD='/home/despoB/connectome-thalamus/MGH'
 for Subject in $(cat ${WD}/usable_sub); do
 
         #if [ ! -e "/home/despoB/kaihwang/Rest/Graph/g_${Subject}.mat" ]; then
-        sed "s/s in Sub0001_Ses1/s in ${Subject}/g" < do_thalamocortical_graph_MGH.sh> tmp/thalamocortical_${Subject}.sh
-        qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/thalamocortical_${Subject}.sh
+        sed "s/s in Sub0001_Ses1/s in ${Subject}/g" < do_striatalcortical_graph_MGH.sh> tmp/striatalcortical_${Subject}.sh
+        qsub -V -M kaihwang -m e -e ~/tmp -o ~/tmp tmp/striatalcortical_${Subject}.sh
         #fi
 
 done
