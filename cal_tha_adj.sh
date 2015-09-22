cb831cbdb4579ca40adda9e5204cc2c4d8bfcd8d
kai.hwang@gmail.com <kaihwang@nx3.priv>
2015-09-22 15:17:22 -0700

    recreate craddock adj matrice for patients.

---
+++ cal_tha_adj.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# script to create adj matrices, or spit out the timeseries file
+
+
+WD='/home/despoB/kaihwang/Rest/Tha_patients'
+
+cd ${WD}
+
+
+for s in 162 163 168 176; do
+
+	mkdir /tmp/KH_${s}/
+	cd ${WD}/${s}/MNINonLinear
+
+
+	## concat
+	if [ -e ${WD}/${s}/MNINonLinear/rfMRI_REST2.nii.gz ]; then
+		3dTcat -prefix rfMRI_REST.nii.gz rfMRI_REST1_PA_reg_bp.nii.gz rfMRI_REST2_PA_reg_bp.nii.gz
+	else
+		3dcopy rfMRI_REST1_PA_reg_bp.nii.gz rfMRI_REST.nii.gz
+
+	fi
+	3dresample -master /home/despoB/connectome-thalamus/ROIs/Craddock_300_cortical.nii.gz \
+	-inset ${WD}/${s}/MNINonLinear/rfMRI_REST.nii.gz \
+	-prefix ${WD}/${s}/MNINonLinear/rfMRI_REST_RPI.nii.gz
+
+	rm ${WD}/${s}/MNINonLinear/rfMRI_REST.nii.gz
+
+	for roi in Craddock_300_cortical Craddock_900_cortical; do
+
+		3dNetCorr \
+		-inset ${WD}/${s}/MNINonLinear/rfMRI_REST_RPI.nii.gz \
+		-in_rois /home/despoB/connectome-thalamus/ROIs/${roi}.nii.gz \
+		-prefix /tmp/KH_${s}/Adj_${roi}
+
+		num=$(expr $(wc -l /tmp/KH_${s}/Adj_${roi}_000.netcc | awk '{print $1}') - 6)
+		tail -n $num /tmp/KH_${s}/Adj_${roi}_000.netcc > /tmp/KH_${s}/Tha_${s}_${roi}_corrmat
+
+		mv /tmp/KH_${s}/Tha_${s}_${roi}_corrmat /home/despoB/connectome-thalamus/NotBackedUp/AdjMatrices
+	done
+
+	rm -rf /tmp/KH_${s}/
+done
