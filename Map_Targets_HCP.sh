a5745505c7401da88ea46fa94334a381f35bfe25
Kai Hwang <kai.hwang@gmail.com>
2017-02-23 11:01:02 -0800

    edit typo on variable.

--- Map_Targets_HCP.sh
+++ Map_Targets_HCP.sh
@@ -8,21 +8,21 @@
 
 cd /home/despoB/connectome-data/
 
-for sub in 100206; do  #$(/bin/ls -d *)
+for s in 100206; do  #$(/bin/ls -d *)
 	
-	mkdir /tmp/${sub}
+	mkdir /tmp/${s}
 
-	cd /home/despoB/connectome-data/${sub}
+	cd /home/despoB/connectome-data/${s}
 	
 	if [ -e rfMRI_REST1_LR/rfMRI_REST1_LR_hp2000_clean_wbsreg.nii.gz -a rfMRI_REST1_RL/rfMRI_REST1_RL_hp2000_clean_wbsreg.nii.gz -a rfMRI_REST2_LR/rfMRI_REST2_LR_hp2000_clean_wbsreg.nii.gz -a rfMRI_REST2_RL/rfMRI_REST2_RL_hp2000_clean_wbsreg.nii.gz ]; then
-		3dTcat -prefix /tmp/${sub}/rest_wbsreg.nii.gz rfMRI_REST1_LR/rfMRI_REST1_LR_hp2000_clean_wbsreg.nii.gz rfMRI_REST1_RL/rfMRI_REST1_RL_hp2000_clean_wbsreg.nii.gz rfMRI_REST2_LR/rfMRI_REST2_LR_hp2000_clean_wbsreg.nii.gz rfMRI_REST2_RL/rfMRI_REST2_RL_hp2000_clean_wbsreg.nii.gz
+		3dTcat -prefix /tmp/${s}/rest_wbsreg.nii.gz rfMRI_REST1_LR/rfMRI_REST1_LR_hp2000_clean_wbsreg.nii.gz rfMRI_REST1_RL/rfMRI_REST1_RL_hp2000_clean_wbsreg.nii.gz rfMRI_REST2_LR/rfMRI_REST2_LR_hp2000_clean_wbsreg.nii.gz rfMRI_REST2_RL/rfMRI_REST2_RL_hp2000_clean_wbsreg.nii.gz
 		
 		for roi in 128_plus_Yeo 162_plus_Yeo 163_plus_Yeo 168_plus_Yeo 176_plus_Yeo Morel_plus_Yeo; do
-			3dNetCorr -prefix ~/tmp/${sub}_${roi} -inset /tmp/${sub}/rest_wbsreg.nii.gz \
+			3dNetCorr -prefix ~/tmp/${s}_${roi} -inset /tmp/${s}/rest_wbsreg.nii.gz \
 			-in_rois /home/despoB/kaihwang/Rest/ThaGate/ROIs/${roi}.nii.gz
 
-			num=$(expr $(wc -l ~/tmp/${sub}_${roi}_000.netcc | awk '{print $1}') - 6)
-			tail -n $num ~/tmp/${sub}_${roi}_000.netcc > /home/despoB/kaihwang/Rest/ThaGate/Matrices/HCP_${sub}_${roi}_corrmat
+			num=$(expr $(wc -l ~/tmp/${s}_${roi}_000.netcc | awk '{print $1}') - 6)
+			tail -n $num ~/tmp/${s}_${roi}_000.netcc > /home/despoB/kaihwang/Rest/ThaGate/Matrices/HCP_${s}_${roi}_corrmat
 		done
 
 	fi	
