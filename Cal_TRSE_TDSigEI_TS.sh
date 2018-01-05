9bbf44da1433c45bbb2c74b7c90f74c0e27afa18
Kai Hwang <kai.hwang@gmail.com>
2018-01-05 14:29:00 -0800

    cleaner ROI set.

--- Cal_TRSE_TDSigEI_TS.sh
+++ Cal_TRSE_TDSigEI_TS.sh
@@ -6,12 +6,12 @@ WD='/home/despoB/kaihwang/TRSE/TDSigEI'
 cd ${WD}
 for s in $(/bin/ls -d 5*); do
 	for data in HF FH Hp Fp Ho Fo; do
-		for roi in Morel_plus_Yeo400_LPI; do 
+		for roi in Morel_Striatum_Yeo400_LPI Morel_Striatum_Gordon_LPI WTA_Striatum_Gordon_LPI; do 
 			3dNetCorr \
-			-inset ${WD}/${s}/${s}_FIR_${data}_errts.nii.gz \
-			-in_rois /home/despoB/kaihwang/Rest/ROIs/${roi}.nii.gz \
+			-inset /home/despoB/kaihwang/TRSE/TDSigEI/NoGS/${s}_FIR_${data}_errts_nogs.nii.gz \
+			-in_rois /home/despoB/kaihwang/Rest/ThaGate/ROIs/${roi}.nii.gz \
 			-ts_out \
-			-prefix /home/despoB/kaihwang/Rest/ThaGate/NotBackedUp/${s}_Morel_plus_Yeo400_${data}
+			-prefix /home/despoB/kaihwang/Rest/ThaGate/NotBackedUp/${s}_${roi}_${data}
 		done
 	done
 done
@@ -22,14 +22,14 @@ done
 WD='/home/despoB/kaihwang/TRSE/TRSEPPI/AllSubjs'
 
 cd ${WD}
-for s in $(cat /home/despoB/kaihwang/bin/TDSigEI/Models/test.subjects); do
+for s in $(cat /home/despoB/kaihwang/bin/TDSigEI/Models/test.subjects); do  #$(cat /home/despoB/kaihwang/bin/TDSigEI/Models/test.subjects)
 	for data in HF FH CAT BO; do
-		for roi in Morel_plus_Yeo400_LPI; do 
+		for roi in Morel_Striatum_Yeo400_LPI Morel_Striatum_Gordon_LPI WTA_Striatum_Gordon_LPI; do 
 			3dNetCorr \
 			-inset ${WD}/${s}/${s}_FIR_${data}_errts.nii.gz \
-			-in_rois /home/despoB/kaihwang/Rest/ROIs/${roi}.nii.gz \
+			-in_rois /home/despoB/kaihwang/Rest/ThaGate/ROIs/${roi}.nii.gz \
 			-ts_out \
-			-prefix /home/despoB/kaihwang/Rest/ThaGate/NotBackedUp/${s}_Morel_plus_Yeo400_${data}
+			-prefix /home/despoB/kaihwang/Rest/ThaGate/NotBackedUp/${s}_${roi}_${data}
 		done
 	done
 done
